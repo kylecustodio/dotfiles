d7bfd396b09131bd0164a9231eb79a09812e6df5
kylecustodio <kkzycg@gmail.com>
2025-07-24 23:05:59 -0500

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# .config files
+ln -sf sway ~/.config/sway
+ln -sf waybar ~/.config/waybar
+ln -sf swaylock ~/.config/swaylock
+ln -sf rofi ~/.config/rofi
+ln -sf dunst ~/.config/dunst
+ln -sf foot ~/.config/foot
+
+# home directory files
+ln -sf .vimrc ~/.vimrc
+ln -sf .zshrc ~/.zshrc
+
