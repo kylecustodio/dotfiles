209ae76d5b2b4d184d045c75531d719765f9d366
kylecustodio <kkzycg@gmail.com>
2025-07-24 23:33:16 -0500

    Add installation nice-to-haves

--- install.sh
+++ install.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
+# vim
+sudo dnf install vim -y
+
+# zsh + change shell
+sudo dnf install zsh -y
+chsh -s $(which zsh)
+
+# tooling
+sudo dnf install zoxide -y
+curl -sS https://starship.rs/install.sh | sh
+
+# install sway + etc
+sudo dnf install sway -y
+sudo dnf swap sway-config sway-config-fedora -y
+sudo dnf install dunst
+
 # .config files
-ln -sf sway ~/.config/sway
-ln -sf waybar ~/.config/waybar
-ln -sf swaylock ~/.config/swaylock
-ln -sf rofi ~/.config/rofi
-ln -sf dunst ~/.config/dunst
-ln -sf foot ~/.config/foot
+cd ~/.config
+ln -sf ~/dotfiles/sway sway
+ln -sf ~/dotfiles/waybar waybar
+ln -sf ~/dotfiles/swaylock swaylock
+ln -sf ~/dotfiles/rofi rofi
+ln -sf ~/dotfiles/dunst dunst
+ln -sf ~/dotfiles/foot foot
 
 # home directory files
-ln -sf .vimrc ~/.vimrc
-ln -sf .zshrc ~/.zshrc
+cd
+ln -sf ~/dotfiles/.vimrc .vimrc
+ln -sf ~/dotfiles/.zshrc .zshrc
 
